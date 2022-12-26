07ab01c715e9f396816e0ede2087634e0b4f6ca7
chanin <chanin.ph@intelligist.co.th>
2022-12-27 01:37:11 +0700

    a

--- server.js
+++ server.js
@@ -1,47 +1,130 @@
 let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
-const res = require('express/lib/response');
+let mysql = require('mysql');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-//homepage route
+// homepage route
 app.get('/', (req, res) => {
     return res.send({ 
         error: false, 
-        message: 'Welcome to RESTful CRUD API with NodeJS, Express, MYSQL'
+        message: 'Welcome to RESTful CRUD API with NodeJS, Express, MYSQL',
     })
 })
 
-app.get('/Hello', (req, res) => {
-    var value = []; //สร้าง Arry 
-    var X = 0;
-    var Y = 0;
-    var Z = 0;
-    var value1 = 3;
-    value.push(value1);
-    for (var i = 1; i < 7; i++) {     
-        value1 = value1+(i*2);
-        value.push(value1)
-        if(i == 4){ 
-            X = value1
-        }else if(i == 5){
-            Y = value1
-        }else if(i == 6){
-            Z = value1
+// connection to mysql database
+let dbConnection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'root',
+    database: 'nodejs_api'
+})
+dbConnection.connect();
+
+// retrieve all books 
+app.get('/getdatafruit', (req, res) => {
+    dbConnection.query('SELECT * FROM books', (error, results, fields) => {
+        if (error) throw error;
+
+        let message = ""
+        if (results === undefined || results.length == 0) {
+            message = "Books table is empty";
+        } else {
+            message = "Successfully retrieved all fruits";
         }
-    }
-    return res.send({ 
-        Value : value,
-        "X": X,
-        "Y": Y,
-        "Z": Z
+        return res.send({ data: results, message: message});
     })
+})
+
+// add a new fruit
+app.post('/addfruit', (req, res) => {
+    let name = req.body.name;
+    let author = req.body.author;
+
+    // validation
+    if (!name || !author) {
+        return res.status(400).send({ message: "Please provide fruit name and author."});
+    } else {
+        dbConnection.query('INSERT INTO books (name, author) VALUES(?, ?)', [name, author], (error, results, fields) => {
+            if (error) throw error;
+            return res.send({message: "fruit successfully added"})
+        })
+    }
+});
+
+// retrieve fruit by name 
+app.get('/getdatafruit/:name', (req, res) => {
+    let name = req.params.name;
+
+    if (!name) {
+        return res.status(400).send({ message: "Please provide fruit name"});
+    } else {
+        dbConnection.query("SELECT * FROM books WHERE name = ?", name, (error, results, fields) => {
+            if (error) throw error;
+
+            let message = "";
+            if (results === undefined || results.length == 0) {
+                message = "fruit not found";
+            } else {
+                message = "Successfully retrieved fruit data";
+            }
+            return res.send({data: results[0], message: message })
+        })
+    }
+})
+
+// update book with id 
+app.put('/updatefruit', (req, res) => {
+    let id = req.body.id;
+    let name = req.body.name;
+    let author = req.body.author;
+
+    // validation
+    if (!id || !name || !author) {
+        return res.status(400).send({ message: 'Please provide fruit id, name and author'});
+    } else {
+        dbConnection.query('UPDATE books SET name = ?, author = ? WHERE id = ?', [name, author, id], (error, results, fields) => {
+            if (error) throw error;
+
+            let message = "";
+            if (results.changedRows === 0) {
+                message = "fruit not found or data are same";
+            } else {
+                message = "fruit successfully updated";
+            }
+
+            return res.send({ error: false, data: results, message: message })
+        })
+    }
+})
+
+// delete fruit by id
+app.delete('/deletefruit', (req, res) => {
+    let id = req.body.id;
+
+    if (!id) {
+        return res.status(400).send({ error: true, message: "Please provide fruit id"});
+    } else {
+        dbConnection.query('DELETE FROM books WHERE id = ?', [id], (error, results, fields) => {
+            if (error) throw error;
+
+            let message = "";
+            if (results.affectedRows === 0) {
+                message = "fruit not found";
+            } else {
+                message = "fruit successfully deleted";
+            }
+
+            return res.send({ error: false, data: results, message: message })
+        })
+    }
 })
 
 app.listen(3000, () => {
-    console.log('Node App is running or port 3000');
+    console.log('Node App is running on port 3000');
 })
 
 module.exports = app;
